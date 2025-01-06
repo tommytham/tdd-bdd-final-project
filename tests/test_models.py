a73d500f7798d836e6f53a8da8178f88a83e2f0a
tommytham <tommytham@hotmail.co.uk>
2025-01-06 07:14:11 -0500

    Initial commit

---
+++ tests/test_models.py
@@ -0,0 +1,154 @@
+# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Test cases for Product Model
+
+Test cases can be run with:
+    nosetests
+    coverage report -m
+
+While debugging just these tests it's convenient to use this:
+    nosetests --stop tests/test_models.py:TestProductModel
+
+"""
+import os
+import logging
+import unittest
+from decimal import Decimal
+from service.models import Product, Category, db
+from service import app
+from tests.factories import ProductFactory
+
+logger = logging.getLogger("flask.app")
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
+)
+
+
+######################################################################
+#  P R O D U C T   M O D E L   T E S T   C A S E S
+######################################################################
+# pylint: disable=too-many-public-methods
+class TestProductModel(unittest.TestCase):
+    """Test Cases for Product Model"""
+
+    @classmethod
+    def setUpClass(cls):
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        Product.init_db(app)
+
+    @classmethod
+    def tearDownClass(cls):
+        """This runs once after the entire test suite"""
+        db.session.close()
+
+    def setUp(self):
+        """This runs before each test"""
+        db.session.query(Product).delete()  # clean up the last tests
+        db.session.commit()
+
+    def tearDown(self):
+        """This runs after each test"""
+        db.session.remove()
+
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################
+
+    def test_create_a_product(self):
+        """It should Create a product and assert that it exists"""
+        product = Product(name="Fedora", description="A red hat", price=12.50, available=True, category=Category.CLOTHS)
+        self.assertEqual(str(product), "<Product Fedora id=[None]>")
+        self.assertTrue(product is not None)
+        self.assertEqual(product.id, None)
+        self.assertEqual(product.name, "Fedora")
+        self.assertEqual(product.description, "A red hat")
+        self.assertEqual(product.available, True)
+        self.assertEqual(product.price, 12.50)
+        self.assertEqual(product.category, Category.CLOTHS)
+
+    def test_add_a_product(self):
+        """It should Create a product and add it to the database"""
+        products = Product.all()
+        self.assertEqual(products, [])
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(product.id)
+        products = Product.all()
+        self.assertEqual(len(products), 1)
+        # Check that it matches the original product
+        new_product = products[0]
+        self.assertEqual(new_product.name, product.name)
+        self.assertEqual(new_product.description, product.description)
+        self.assertEqual(Decimal(new_product.price), product.price)
+        self.assertEqual(new_product.available, product.available)
+        self.assertEqual(new_product.category, product.category)
+
+    def test_read_a_product(self):
+        """It should Read a product from the database"""
+        product = ProductFactory()
+        logger.info("Product from ProductFactory: %s", product)
+        product.id = None
+        product.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(product.id)
+        product_from_db = Product.find(product.id)
+        # Assert values from creation matches what has been fetched
+        self.assertEqual(product_from_db.id, product.id)
+        self.assertEqual(product_from_db.name, product.name)
+        self.assertEqual(product_from_db.description, product.description)
+        self.assertEqual(product_from_db.price, product.price)
+        self.assertEqual(product_from_db.available, product.available)
+        self.assertEqual(product_from_db.category, product.category)
+
+    def test_update_a_product(self):
+        """It should Update a product from the database"""
+        product = ProductFactory()
+        logger.info("Product from ProductFactory: %s", product)
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        logger.info("Product created in the database: %s", product)
+        original_id = product.id
+        product.description = "This product has an updated description"
+        product.update()
+        self.assertEqual(product.id, original_id)
+        self.assertEqual(product.description, "This product has an updated description")
+        # Fetch all products from the database and check there is only one record with correct information
+        products = Product.all()
+        self.assertEqual(len(products), 1)
+        self.assertEqual(products[0].id, original_id)
+        self.assertEqual(products[0].description, "This product has an updated description")
+
+    def test_delete_a_product(self):
+        """It should Delete a product from the database"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        product.delete()
+        # Assert that no products exists in the database
+        products = Product.all()
+        self.assertEqual(len(products), 0)
+    #
+    # ADD YOUR TEST CASES HERE
+    #
